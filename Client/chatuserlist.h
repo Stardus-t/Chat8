a2360a7bfa4636d2f08ba067f35d4ece7080ecb8
Stardus-t <869003158@qq.com>
2025-10-05 10:11:21 +0800

    Add files via upload

---
+++ Client/chatuserlist.h
@@ -0,0 +1,21 @@
+#ifndef CHATUSERLIST_H
+#define CHATUSERLIST_H
+#include <QListWidget>
+#include <QString>
+
+class ChatUserList:public QListWidget
+{
+    Q_OBJECT
+public:
+    ChatUserList(QWidget *parent=nullptr);
+    void addChatUser(QString Username);
+
+protected:
+    bool eventFilter(QObject *watched,QEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+signals:
+    void sig_loadding_chat_user();
+};
+
+#endif // CHATUSERLIST_H
