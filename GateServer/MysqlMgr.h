0fdf2f91add601ad66ef1f22e2536ec60e72a8bb
Stardus-t <869003158@qq.com>
2025-10-13 22:12:49 +0800

    Add files via upload

---
+++ GateServer/MysqlMgr.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "const.h"
+#include "MysqlDao.h"
+#include "Singleton.h"
+
+class MysqlMgr : public Singleton<MysqlMgr>
+{
+	friend class Singleton<MysqlMgr>;
+public:
+	~MysqlMgr();
+
+	int RegUser(const std::string& name, const std::string& email, const std::string& pwd, const std::string& icon);
+	bool CheckEmail(const std::string& name, const std::string& email);
+	bool UpdatePwd(const std::string& name, const std::string& email);
+	bool CheckPwd(const std::string& email, const std::string& pwd, UserInfo& userInfo);
+	bool TestProcedure(const std::string& email, int& uid, std::string& name);
+	bool GetUid(const std::string& email, int& uid);
+private:
+	MysqlMgr();
+	MysqlDao  _dao;
+};
+
